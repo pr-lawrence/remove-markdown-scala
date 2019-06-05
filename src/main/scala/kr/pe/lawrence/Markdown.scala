8cb322d4ef61d2c401fcf259752eadeb155682bd
kLawrence <ultimate1352@gmail.com>
2019-06-06 01:44:54 +0900

    enhance

--- src/main/scala/kr/pe/lawrence/Markdown.scala
+++ src/main/scala/kr/pe/lawrence/Markdown.scala
@@ -9,8 +9,14 @@ package kr.pe.lawrence
   */
 object Markdown {
   def remove(content: String): String = {
+
+    // Remove horizontal rules (stripListHeaders conflict with this rule, which is why it has been moved to the top)
+    var output = content.replaceAll("(?m)^(-\\s*?|\\*\\s*?|_\\s*?){3,}\\s*$", "")
+
+    output = output.replaceAll("(?m)^([\\s\\t]*)([\\*\\-\\+]|\\d+\\.)\\s+", "$1")
+
     // Remove HTML tags
-    content.replaceAll("<[^>]*>", "")
+    output.replaceAll("<[^>]*>", "")
       // Remove setext-style headers
       .replaceAll("^[=\\-]{2,}\\s*$", "")
       // Remove footnotes?
@@ -25,12 +31,12 @@ object Markdown {
       // Remove reference-style links?
       .replaceAll("^\\s{1,2}\\[(.*?)\\]: (\\S+)( \".*?\")?\\s*$", "")
       // Remove atx-style headers
-      .replaceAll("^(\\n)?\\s{0,}#{1,6}\\s+| {0,}(\\n)?\\s{0,}#{0,} {0,}(\\n)?\\s{0,}$", "$1$2$3")
+      .replaceAll("(?m)^(\\n)?\\s{0,}#{1,6}\\s+| {0,}(\\n)?\\s{0,}#{0,} {0,}(\\n)?\\s{0,}$", "$1$2$3")
       // Remove emphasis (repeat the line to remove double emphasis)
       .replaceAll("([\\*_]{1,3})(\\S.*?\\S{0,1})\\1", "$2")
       .replaceAll("([\\*_]{1,3})(\\S.*?\\S{0,1})\\1", "$2")
       // Remove code blocks
-      .replaceAll("(`{3,})(.*?)\\1", "$2")
+      .replaceAll("(`{3,})(.*)", "")
       // Remove inline code
       .replaceAll("`(.+?)`", "$1")
       // Replace two or more newlines with exactly two? Not entirely sure this belongs here...
--- src/test/scala/kr/pe/lawrence/MarkdownSpec.scala
+++ src/test/scala/kr/pe/lawrence/MarkdownSpec.scala
@@ -1,6 +1,5 @@
 package kr.pe.lawrence
 
-import collection.mutable.Stack
 import org.scalatest._
 
 /**
@@ -15,6 +14,29 @@ class MarkdownSpec extends FlatSpec with Matchers {
   "Markdown" should "remove" in {
     val result = """<a href="http://www.google.com">Google</a> ![image](http://www.google.com/image) [Facebook][http://www.facebook.com]""".replaceAll("<[^>]*>", "")
     Markdown.remove(result) should be("Google  Facebook")
+  }
+
+  "Markdown" should "remove2" in {
+    val content = "## 트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다.\n\n" +
+    "## 트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다.\n\n" +
+    "## 트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다.\n\n" +
+    "## 트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다.\n\n" +
+      "- 1\n" +
+      "- 2\n" +
+      "- 3\n" +
+      "- 4\n" +
+      "\n" +
+      "트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다. 트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다. 트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다. 트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다. 트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다. 트레이트는 자바에서 인터페이스와 같은 역할을 하는 스칼라의 키워드로 Trait로 선언한다.\n" +
+      "\n" +
+      "```scala\n" +
+      "val a = 3\n" +
+      "val b = 4\n" +
+      "val sum = a + b\n" +
+      "```\n" +
+      "\n" +
+      "blablabla"
+
+      Console println Markdown.remove(content)
   }
 
   /*
